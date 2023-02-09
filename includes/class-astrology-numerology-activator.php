6ea2f2eb4689285740c09ac4249d854cc79ec893
shaon-hossain45 <shaonhossain915@gmail.com>
2023-02-09 13:28:03 +0600

    Start the plugin development

---
+++ includes/class-astrology-numerology-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/shaon-hossain45/
+ * @since      1.0.0
+ *
+ * @package    Astrology_Numerology
+ * @subpackage Astrology_Numerology/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Astrology_Numerology
+ * @subpackage Astrology_Numerology/includes
+ * @author     Shaon Hossain <shaonhossain615@gmail.com>
+ */
+class Astrology_Numerology_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-astrology-numerology-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://github.com/shaon-hossain45/
+ * @since      1.0.0
+ *
+ * @package    Astrology_Numerology
+ * @subpackage Astrology_Numerology/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Astrology_Numerology
+ * @subpackage Astrology_Numerology/includes
+ * @author     Shaon Hossain <shaonhossain615@gmail.com>
+ */
+class Astrology_Numerology_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
